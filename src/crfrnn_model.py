57344b010aa7d0ffbfcb0627829171d16114a4ca
Mohamed Adham Mahrous <s-mohamed.adham@zewailcity.edu.eg>
2018-09-10 16:46:46 +0200

    Update crfrnn_model.py
    Add trainability

--- src/crfrnn_model.py
+++ src/crfrnn_model.py
@@ -46,60 +46,60 @@ def get_crfrnn_model_def():
     x = ZeroPadding2D(padding=(100, 100))(img_input)
 
     # VGG-16 convolution block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv1_1')(x)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_2')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='valid', name='conv1_1',trainable=False)(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_2',trainable=False)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(x)
 
     # VGG-16 convolution block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool2', padding='same')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_1', trainable=False)(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_2', trainable=False)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool2', padding='same', trainable=False)(x)
 
     # VGG-16 convolution block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool3', padding='same')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_1', trainable=False)(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_2', trainable=False)(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_3', trainable=False)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool3', padding='same', trainable=False)(x)
     pool3 = x
 
     # VGG-16 convolution block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool4', padding='same')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_1', trainable=False)(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_2', trainable=False)(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_3', trainable=False)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool4', padding='same', trainable=False)(x)
     pool4 = x
 
     # VGG-16 convolution block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_3')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool5', padding='same')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_1', trainable=False)(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_2', trainable=False)(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_3', trainable=False)(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool5', padding='same', trainable=False)(x)
 
     # Fully-connected layers converted to convolution layers
-    x = Conv2D(4096, (7, 7), activation='relu', padding='valid', name='fc6')(x)
+    x = Conv2D(4096, (7, 7), activation='relu', padding='valid', name='fc6', trainable=True)(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(4096, (1, 1), activation='relu', padding='valid', name='fc7')(x)
+    x = Conv2D(4096, (1, 1), activation='relu', padding='valid', name='fc7', trainable=True)(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(21, (1, 1), padding='valid', name='score-fr')(x)
+    x = Conv2D(21, (1, 1), padding='valid', name='score-fr', trainable=True)(x)
 
     # Deconvolution
-    score2 = Conv2DTranspose(21, (4, 4), strides=2, name='score2')(x)
+    score2 = Conv2DTranspose(21, (4, 4), strides=2, name='score2', trainable=True)(x)
 
     # Skip connections from pool4
-    score_pool4 = Conv2D(21, (1, 1), name='score-pool4')(pool4)
+    score_pool4 = Conv2D(21, (1, 1), name='score-pool4', trainable=True)(pool4)
     score_pool4c = Cropping2D((5, 5))(score_pool4)
     score_fused = Add()([score2, score_pool4c])
-    score4 = Conv2DTranspose(21, (4, 4), strides=2, name='score4', use_bias=False)(score_fused)
+    score4 = Conv2DTranspose(21, (4, 4), strides=2, name='score4', use_bias=False, trainable=True)(score_fused)
 
     # Skip connections from pool3
-    score_pool3 = Conv2D(21, (1, 1), name='score-pool3')(pool3)
+    score_pool3 = Conv2D(21, (1, 1), name='score-pool3', trainable=True)(pool3)
     score_pool3c = Cropping2D((9, 9))(score_pool3)
 
     # Fuse things together
     score_final = Add()([score4, score_pool3c])
 
     # Final up-sampling and cropping
-    upsample = Conv2DTranspose(21, (16, 16), strides=8, name='upsample', use_bias=False)(score_final)
+    upsample = Conv2DTranspose(21, (16, 16), strides=8, name='upsample', use_bias=False, trainable=True)(score_final)
     upscore = Cropping2D(((31, 37), (31, 37)))(upsample)
 
     output = CrfRnnLayer(image_dims=(height, weight),
